95941e6d389e1778feddde2a0eeb6ed9d9970bbb
OswaldLim <limchuanhe@gmail.com>
2026-04-25 15:39:41 +0800

    gave allowed accesd from vercel to index.py fasapi

--- api/index.py
+++ api/index.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI, HTTPException, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from src import rag_pipeline  # Assuming your functions are in src/rag_pipeline.py
 from src import utils
 import os
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Define the request structure
 class QueryRequest(BaseModel):
